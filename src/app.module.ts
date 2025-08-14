f335773e6fa6c02be4a7073e1ee8eedfe07690cf
Filipe <filipelehmannp@gmail.com>
2025-08-14 16:26:26 -0300

    Initialized application with NestJS CLI

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { CatsModule } from './resources/cats/cats.module';
+import { CatsModule } from './cats/cats.module';
+
+@Module({
+  imports: [CatsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
